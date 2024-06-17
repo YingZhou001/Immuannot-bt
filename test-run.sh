5e1900719ed146991d4031c7b9275c2189cda9ca
YingZhou001 <31318632+YingZhou001@users.noreply.github.com>
2024-06-17 14:16:42 -0400

    Update test-run.sh

--- test-run.sh
+++ test-run.sh
@@ -6,20 +6,11 @@ outpref=test-out
 asm=Immuannot-bt/test-data/sel-asm.fa.gz
 reads=Immuannot-bt/test-data/sel-reads.fa.gz
 
-if [ ${asm} == unset ]
-then
-  echo "target assembly input is missing"
-else 
-  mm2thread=3
-  echo "[msg:] testing assembly-based annotation"
-  time bash ${scriptdir}/immuannot-bt.sh -a ${asm} -d ${refdir} -o ${outpref} -t ${mm2thread}
-fi
 
-if [ ${reads} == unset ]
-then
-  echo "target reads input is missing"
-else
-  mm2thread=10
-  echo "[msg:] testing annotating HiFi reads for V(D)J events"
-  time bash ${scriptdir}/immuannot-bt.sh -r ${reads} -d ${refdir} -o ${outpref} -t ${mm2thread}
-fi
+mm2thread=3
+echo "[msg:] testing assembly-based annotation"
+time bash ${scriptdir}/immuannot-bt.sh -a ${asm} -d ${refdir} -o ${outpref} -t ${mm2thread}
+
+echo "[msg:] testing annotating HiFi reads for V(D)J events"
+time bash ${scriptdir}/immuannot-bt.sh -r ${reads} -d ${refdir} -o ${outpref} -t ${mm2thread}
+
